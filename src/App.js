c6bde439acb5fbb57da83423805afbcc4a828676
Taryn Wou <75547547+tarynwou@users.noreply.github.com>
2022-05-06 01:38:20 -0700

    Set up Advice.jsx functionality

--- src/App.js
+++ src/App.js
@@ -4,13 +4,26 @@ import "./App.css";
 function App() {
 	return (
 		<div>
-			<h1>Grains of Salt</h1>
+			<h1 id="title">Grains of Salt</h1>
+			<p id="subtitle">
+				A compilation of advice I have received from good-natured
+				people.
+			</p>
 
 			<div className="page-column" id="left-column">
-				<Advice short="hi" long="goodbye"></Advice>
+				<h2 className="category-title" id="lifestyle">
+					Lifestyle
+				</h2>
+				<Advice
+					short="If you're feeling bad, HALT. Check if you're hungry, angry, lonely, or tired."
+					long="goodbye"
+				></Advice>
 			</div>
 
 			<div className="page-column" id="right-column">
+				<h2 className="category-title" id="school">
+					School
+				</h2>
 				<Advice short="hello" long="goodbye"></Advice>
 			</div>
 		</div>
--- src/components/Advice.jsx
+++ src/components/Advice.jsx
@@ -1,29 +1,38 @@
 import React from "react";
+import { useState } from "react";
 import "./components.css";
 
 function Advice(props) {
-	// useState line
+	const [toggleOpen, toggle] = useState(false);
 
 	// if untoggled
 	// return short advice
-	return (
-		<div className="advice-toggle-short">
-			<p className="advice-short-closed">{props.short}</p>
-			<img src="" alt="" />
-		</div>
-	);
-
-	// if toggled
-	// return long advice
-	// return (
-	// 	<div className="advice-toggle-short">
-	// row 1
-	// 		<p className="advice-short-open">{props.short}</p>
-	// 		<img src="" alt="" />
-	// row 2
-	//  	<p className="advice-long">{props.long}</p>
-	// 	</div>
-	// );
+	if (toggleOpen) {
+		return (
+			<div
+				className="advice-toggle-short"
+				onClick={() => toggle(!toggleOpen)}
+			>
+				<p className="advice-short-closed">{props.short}</p>
+				<img src="" alt="" />
+			</div>
+		);
+	} else {
+		// if toggled
+		// return long advice
+		return (
+			<div
+				className="advice-toggle-long"
+				onClick={() => toggle(!toggleOpen)}
+			>
+				{/* row 1 */}
+				<p className="advice-short-open">{props.short}</p>
+				<img src="" alt="" />
+				{/* row 2 */}
+				<p className="advice-long">{props.long}</p>
+			</div>
+		);
+	}
 }
 
 export default Advice;
